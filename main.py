833542f4347a96d7e5dab44b9650c84fd729cfdd
Trishant <trishantjain4444@gmail.com>
2023-02-19 23:53:37 +0530

    added order taking function

--- main.py
+++ main.py
@@ -17,25 +17,10 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 token = "6258989185:AAEzPzeOPue371Qbbsz15MHSDTlSfuM0WUI"
 freshner_list = ["Bonanza", "Lemon", "Grapes", "Mint"]
-ADDRESS, PHONE = range(2)
+ADDRESS, PHONE, FRESHNER_LIST, QUANTITY = range(4)
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
-
-
-# def help(update, context):
-#     update.message.reply_text('''
-#         /profile - Profile of our business.
-# /freshners - list of room freshners.
-#        ''')
-
-
-# def freshners(update, context):
-#     update.message.reply_text('''
-#         1. Bonanza
-# 2. Lemon
-# 3. Grapes
-#     ''')
 
 
 # def order(update: Update, context):
@@ -64,7 +49,6 @@ def set_address(update, context):
     address = update.message.text
     context.user_data['address'] = address
     context.user_data['user_name'] = user_name
-    update.message.reply_text("Address set as: " + address)
     update.message.reply_text("Please enter your phone number: ")
     return PHONE
 
@@ -73,14 +57,7 @@ def set_phone(update, context):
     global phone
     phone = update.message.text
     context.user_data['phone'] = phone
-    #? Storing 'address' and 'phone' in Database 
-    user_data = {
-        "user_name": context.user_data.get('user_name'),
-        "address": context.user_data.get('address'),
-        "phone": phone
-    }
-    update.message.reply_text("Phone set as: " + phone)
-    mycollection.insert_one(user_data)
+    update.message.reply_text("type /order to get list of Room freshners.")
     return ConversationHandler.END
 
 
@@ -88,6 +65,33 @@ def cancel(update, context):
     update.message.reply_text('Command canceled.')
     return ConversationHandler.END
 
+
+def order(update, context):
+    update.message.reply_text("Please select a freshner from the list or type /cancel to cancel the procedure." +
+                              "\n".join(f"{i+1}, {freshner}" for i, freshner in enumerate(freshner_list)))
+    return FRESHNER_LIST
+
+
+def set_freshner(update, context):
+    selected_freshner = update.message.text
+
+    if selected_freshner.lower() == "/order":
+        update.message.reply_text("Order Cancelled")
+        return ConversationHandler.END
+    
+
+    context.user_data["selected_freshner"] = selected_freshner
+    order_details = {
+        "user_name": context.user_data.get('user_name'),
+        "address": context.user_data.get('address'),
+        "phone": context.user_data.get('phone'),
+        'selected_freshner': context.user_data.get('selected_freshner')
+    }
+    mycollection.insert_one(order_details)
+    update.message.reply_text(
+        f"This is your order. \n{selected_freshner}")
+    # return QUANTITY
+    return ConversationHandler.END
 
 def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
@@ -113,7 +117,17 @@ def main():
         fallbacks=[CommandHandler('cancel', cancel)]
     )
 
+    conv_handler2 = ConversationHandler(
+        entry_points=[CommandHandler('order', order)],
+        states={
+            FRESHNER_LIST: [MessageHandler(Filters.text, set_freshner)],
+            # QUANTITY: [MessageHandler(Filters.text, set_quantity)]
+        },
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
+
     dp.add_handler(conv_handler)
+    dp.add_handler(conv_handler2)
 
     # start the bot
     updater.start_polling()
